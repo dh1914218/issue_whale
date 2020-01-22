f7ec32532109f82fc6fdcdbf10afa55c1f8b6124
dh1914218 <dh1914218@gmail.com>
2020-01-22 17:10:55 +0900

    ㅎ

--- js/script.js
+++ js/script.js
@@ -22,14 +22,124 @@ $(document).ready(function() {
   });
 });
 
-//----------vote----------
-// --찬반투표 탭 클릭시
+$(".vote__tab-left .btn__item--3").click(function() {
+  console.log(1);
+});
+
+//------------------vote----------------
+
+function test1() {
+  // var test1 = $("vote__tab-left ul li.btn__item--3 img");
+
+  $("vote__tab-left ul li.btn__item--3 img").attr("src", function(index, attr) {
+    if (attr.match("_off.svg")) {
+      return attr.replace("_off.svg", "_on.svg");
+    } else {
+      return attr.replace("_on.svg", "_off.svg");
+    }
+  });
+}
+
+// --탭클릭시
+
+/*
+let voteTabList = 0;
+$(".vote__tab-left ul li").click(function() {
+  voteTabList = $(this).index();
+
+  // 첫번째버튼
+  if (voteTabList == 0) {
+    $(".vote__tab-left ul li")
+      .find("span")
+      .removeClass("font__active__purple_400");
+    $(this)
+      .find("span")
+      .addClass("font__active__purple_400");
+
+    //이미지 - 기존것지우기
+    $(".vote__tab-left .btn__item--2")
+      .find("img")
+      .attr("src", "img/ic_tab-yes_off.svg");
+
+    $(".vote__tab-left .btn__item--3")
+      .find("img")
+      .attr("src", "img/ic_tab-no_off.svg");
+
+    // 이미지 - 클릭시 변경
+    $(".vote__tab-left ul li.btn__item--1")
+      .find("img")
+      .attr(
+        "src",
+        $(this)
+          .find("img")
+          .attr("src")
+          .replace("_off.svg", "_on.svg")
+      );
+  }
+
+  // 두번째버튼
+  else if (voteTabList == 1) {
+    $(".vote__tab-left ul li")
+      .find("span")
+      .removeClass("font__active__purple_400");
+    $(this)
+      .find("span")
+      .addClass("font__active__purple_400");
+
+    $(".vote__tab-left .btn__item--1")
+      .find("img")
+      .attr("src", "img/ic_tab-all_off.svg");
+
+    $(".vote__tab-left .btn__item--3")
+      .find("img")
+      .attr("src", "img/ic_tab-no_off.svg");
+
+    $(".vote__tab-left ul li.btn__item--2")
+      .find("img")
+      .attr(
+        "src",
+        $(this)
+          .find("img")
+          .attr("src")
+          .replace("_off.svg", "_on.svg")
+      );
+
+    // 세번째버튼
+  } else {
+    $(".vote__tab-left ul li")
+      .find("span")
+      .removeClass("font__active__purple_400");
+    $(this)
+      .find("span")
+      .addClass("font__active__purple_400");
+
+    //이미지 - 기존것지우기
+    $(".vote__tab-left .btn__item--1")
+      .find("img")
+      .attr("src", "img/ic_tab-all_off.svg");
+
+    $(".vote__tab-left .btn__item--2")
+      .find("img")
+      .attr("src", "img/ic_tab-yes_off.svg");
+
+    // 이미지 - 클릭시 변경
+    $(".vote__tab-left ul li.btn__item--3")
+      .find("img")
+      .attr(
+        "src",
+        $(this)
+          .find("img")
+          .attr("src")
+          .replace("_off.svg", "_on.svg")
+      );
+  }
+});
+*/
 
 // --찬반투표 투표버튼 클릭시
 let voteGraphBtn = 0;
 $(".vote__main .btn ul li").click(function() {
   voteGraphBtn = $(this).index();
-  console.log(1);
 
   if (voteGraphBtn == 0) {
     $(".bgblack-layerpopup").show();
